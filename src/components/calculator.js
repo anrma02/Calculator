8a4021c91663cb8ce9a59ea9ec215c8da98c6d8a
An Madridstar <internship>
2024-03-17 19:41:46 +0700

    Update Logic Calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,27 +1,30 @@
+// Hàm để loại bỏ khoảng trắng và chuyển đổi "+-" thành "-"
+const cleanExpression = (expression) => {
+     return expression.replace(/\s+/g, '').replace(/\+\\-/g, '-');
+};
+
+// Hàm để kiểm tra xem một ký tự có phải là một số hay không
+const isNumeric = (char) => {
+     return !isNaN(char) || char === '.';
+};
+
 /**
- * @param {string} expression - Biểu thức cần đánh giá
- * @returns {string} - Kết quả của biểu thức
+ *
+ *  @param {string} expression - Biểu thức cần phân tách
+ * @returns {object} - Một object chứa mảng các số và mảng các toán tử
  */
 
-export const calculate = (expression) => {
-     expression = expression.replace(/\s+/g, '');
+// Hàm để phân tách biểu thức thành các số và toán tử
 
-     expression = expression.replace(/\+\\-/g, '-');
-
-     if (expression[0] === '+') {
-          expression = expression.slice(1);
-     }
+const parseExpression = (expression) => {
      const numbers = [];
      const operators = [];
-
      let currentNumber = '';
+
      for (let i = 0; i < expression.length; i++) {
           const char = expression[i];
-          if (
-               !isNaN(char) ||
-               char === '.' ||
-               (char === '-' && (i === 0 || isNaN(expression[i - 1])))
-          ) {
+
+          if (isNumeric(char) || (char === '-' && (i === 0 || isNaN(expression[i - 1])))) {
                currentNumber += char;
           } else {
                if (currentNumber) {
@@ -40,10 +43,26 @@ export const calculate = (expression) => {
           throw new Error('Invalid expression');
      }
 
+     return { numbers, operators };
+};
+
+/**
+ *
+ * @param {array} numbers - Mảng chứa các số
+ * @param {array} operators - Mảng chứa các toán tử
+ * @returns {number} - Kết quả của biểu thức
+ * @throws {Error} - Nếu có lỗi trong quá trình tính toán
+ *
+ */
+
+// Hàm để tính toán kết quả từ các số và toán tử
+const computeResult = (numbers, operators) => {
      let result = numbers[0];
+
      for (let i = 0; i < operators.length; i++) {
           const operator = operators[i];
           const nextNumber = numbers[i + 1];
+
           switch (operator) {
                case '+':
                     result += nextNumber;
@@ -58,17 +77,29 @@ export const calculate = (expression) => {
                     if (nextNumber === 0) {
                          throw new Error('Divide by zero error');
                     }
-                    result /= nextNumber;
+                    result = parseFloat((result / nextNumber).toFixed(3));
                     break;
                default:
                     throw new Error('Unknown operator');
           }
      }
+
+     return result;
+};
+
+/**
+ * @param {string} expression - Biểu thức cần tính toán
+ * @returns {string} - Kết quả của biểu thức
+ */
+
+export const calculate = (expression) => {
+     expression = cleanExpression(expression);
+     const { numbers, operators } = parseExpression(expression);
+     const result = computeResult(numbers, operators);
      const formattedResult = String(result).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, '$1 ');
 
      return formattedResult;
 };
-
 /**
  *
  * @param {string} displayValue - Giá trị hiển thị trên màn hình
